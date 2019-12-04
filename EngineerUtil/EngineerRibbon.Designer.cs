5142a385a6537345a865598cf23583b3eabc68a8
Himmelt <master@void-3.cn>
2019-12-04 18:04:30 +0800

    first

---
+++ EngineerUtil/EngineerRibbon.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace EngineerUtil
+{
+    partial class EngineerRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public EngineerRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.btnPLC = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.btnPLC);
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // btnPLC
+            // 
+            this.btnPLC.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnPLC.Label = "btnPLC";
+            this.btnPLC.Name = "btnPLC";
+            this.btnPLC.ShowImage = true;
+            this.btnPLC.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnPLC_Click);
+            // 
+            // EngineerRibbon
+            // 
+            this.Name = "EngineerRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.EngineerRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnPLC;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal EngineerRibbon EngineerRibbon {
+            get { return this.GetRibbon<EngineerRibbon>(); }
+        }
+    }
+}
---
+++ EngineerUtil/EngineerRibbon.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Tools.Ribbon;
+using Application = Microsoft.Office.Interop.Excel.Application;
+
+namespace EngineerUtil
+{
+    public partial class EngineerRibbon
+    {
+
+        private Application application;
+
+        private void EngineerRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+            application = Globals.ThisAddIn.Application;
+        }
+
+        private void btnPLC_Click(object sender, RibbonControlEventArgs e)
+        {
+            Workbook workbook = application.ActiveWorkbook;
+            try
+            {
+                Worksheet plcSheet = workbook.Sheets["PLC_Engineers_Plan"];
+                Worksheet plcSheet2 = workbook.Sheets["PLC_Engineers_Resume"];
+                Range Cells = plcSheet.Cells;
+                int startRow = 0, startCol = 0, endRow = 0, endCol = 0;
+
+                if (plcSheet != null && plcSheet2 != null)
+                {
+                    bool findRange = false;
+                    for (int i = 1; i <= 800; i++)
+                    {
+                        for (int j = 1; j <= 100; j++)
+                        {
+                            //Cells[i, j].Value2 = Convert.ToString(i + j);
+                            var value = Cells[i, j].Value2;
+                            if (value!=null && value.ToString().Equals("MARK_START"))
+                            {
+                                startRow = i;
+                                startCol = j;
+                            }
+                            else if (value != null && value.ToString().Equals("MARK_END"))
+                            {
+                                endRow = i;
+                                endCol = j;
+                                findRange = true;
+                                break;
+                            }
+                        }
+                        if (findRange) break;
+                    }
+
+                    if (findRange)
+                    {
+                        Range Cells2 = plcSheet2.Cells;
+                        int k = 1;
+                        var name = Cells2[15 * (k - 1) + 1, 3].Value2;
+                        
+                        while(name !=null )
+                        {
+                            string theName = name.ToString();
+
+                            Dictionary<int, List<int>> map = new Dictionary<int, List<int>>();
+
+                            for(int m = startRow; m <= endRow; m++)
+                            {
+                                for(int n = startCol; n <= endCol; n++)
+                                {
+                                    var value = Cells[m, n].Value2;
+                                    if (value != null && value.ToString().Equals(theName))
+                                    {
+                                        List<int> list = null;
+                                        if (map.TryGetValue(m, out list)) {
+                                            map[m].Add(n);
+                                        }
+                                        else
+                                        {
+                                            list = new List<int>();
+                                            list.Add(n);
+                                            map.Add(m,list);
+                                        }
+                                    }
+                                }
+                            }
+
+
+                            int l = 1;
+                            // 先把14行清空
+                            for (l = 1; l <= 14; l++)
+                            {
+                                Cells2[15 * (k - 1) + 1 + l, 4].Value2 = null;
+                                Cells2[15 * (k - 1) + 1 + l, 5].Value2 = null;
+                            }
+
+                            l = 1;
+
+                            var sort = from obj in map orderby obj.Key ascending select obj;
+
+                            foreach (KeyValuePair<int, List<int>> kvp in sort)
+                            {
+                                if (l <= 14)
+                                {
+                                    int line = kvp.Key;
+                                    List<int> list = kvp.Value;
+                                    string result = "KW " + list.First() + "-" + list.Last();
+                                    Cells2[15 * (k - 1) + 1 + l, 4].Value2 = result;
+                                    Range range = Cells[line, startCol + 3];
+                                    Cells2[15 * (k - 1) + 1 + l, 5].Value2 = range.MergeArea.Cells[1, 1].Value2;
+                                }
+                                l++;
+                            }
+
+                            k++;
+                            name = Cells2[15 * (k - 1) + 1, 3].Value2;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not found valid range mark !!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("PLC Engineers workSheet not exist !!");
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("PLC Engineers workSheet not exist !!");
+                MessageBox.Show($"Error message: {ex.Message}\n" + $"Details:\n{ex.StackTrace}");
+            }
+        }
+    }
+}
